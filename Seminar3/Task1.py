fc04c6b46e2dd7114cc80f58fa4d1626567da647
Yakov Lozhkin <yasher89@mail.ru>
2022-12-31 11:26:16 +0400

    Task1

---
+++ Seminar3/Task1.py
@@ -0,0 +1,26 @@
+# 1. Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+
+# Пример:
+# - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+import random
+
+num = int(input("Какое количество чисел будет в списке? -> "))
+
+def Random_List(namber: int):
+    lst = [random.randint(1,10) for i in range(namber)]
+    return lst
+
+my_list = Random_List(num)
+print (my_list)
+
+# 1-й вариант
+summ = 0
+for i in range(len(my_list)):
+    if (i%2!=0):
+        summ += int(my_list[i])
+print (f'Сумма элементов списка, стоящих на нечётной позиции {summ}')
+
+# 2-й вариант
+new_list = [my_list[x] for x in range(len(my_list)) if x % 2]
+print(f'Сумма элементов списка, стоящих на нечётной позиции {sum(new_list)}')
