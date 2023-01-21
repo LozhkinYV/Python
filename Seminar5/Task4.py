023c6b64b2f710cae7b2f2c85ed5bb669568de44
Yakov Lozhkin <yasher89@mail.ru>
2023-01-21 18:52:53 +0400

    Task2

---
+++ Seminar5/Task4.py
@@ -0,0 +1,43 @@
+# 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных
+
+def encode(text: str) -> str:
+    encoded = '' 
+    previous = '' 
+    count = 1
+    if not text: 
+        return '' 
+    for char in text: 
+        if char != previous: 
+            if previous: 
+                encoded += str(count) + previous 
+            count = 1 
+            previous = char 
+        else: 
+            count += 1 
+    else: 
+        encoded += str(count) + previous 
+    return encoded
+
+def decode(encodedstring):
+    decode = ''
+    count = ''
+    for char in encodedstring:
+        if char.isdigit():
+            count += char
+        else:
+            decode += char * int(count)
+            count = ''
+    return decode
+
+print('Изначальная строка:')
+my_text = 'AAAAZZZZZZZZZZZZZxxRRRRRTTTTYHHHHHHHHHHHHHHKKKKKKKK'
+print(my_text)
+print()
+print('Сжатая строка:')
+new_text = encode(my_text) 
+print(new_text)
+print()
+print('Распакованная строка:')
+my_text = decode(new_text)
+print(my_text)
+ 
